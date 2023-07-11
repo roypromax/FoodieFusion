d15c50d8d2b547658724b5ffe31e2906fb0a499c
roypromax <roy19rishabh@gmail.com>
2023-07-12 00:46:52 +0530

    Backend integrated with mongoDB

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,37 @@
 from flask import Flask, jsonify, request
-import json
 import random
+import os
 from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
+
+mongo_url = os.environ.get('MONGO_URL')
+client = MongoClient(mongo_url)
+db = client['foodieFusion']
+
 
 @app.route("/", methods=["GET"])
 def home_route():
-    return jsonify("Welcome to the FoodieFiesta order management app.")
+    return jsonify("Welcome to the FoodieFusion order management app.")
 
 
 @app.route("/menu/read", methods=["GET"])
 def read_menu():
-    with open("menu.json", "r") as file:
-        menu = json.load(file)
-
-    return jsonify(menu), 200
+    menu = list(db.menu.find())
+    menu_list = []
+    for item in menu:
+        menu_list.append({
+            'id': item['id'],
+            'name': item['name'],
+            'price': item['price'],
+            'availability': item['availability']
+        })
+    return jsonify(menu_list), 200
 
 
 @app.route("/menu/create", methods=["POST"])
@@ -27,17 +41,12 @@ def create_menu_item():
     if "id" not in new_menu_item or "name" not in new_menu_item or "price" not in new_menu_item or "availability" not in new_menu_item:
         return jsonify({"error": "Invalid menu item data"}), 400
 
-    with open("menu.json", "r") as file:
-        menu = json.load(file)
+    existing_menu_item = db.menu.find_one({"id": new_menu_item["id"]})
 
-    for item in menu:
-        if item["id"] == new_menu_item["id"]:
-            return jsonify({"error": "Menu item ID already exists"}), 400
+    if existing_menu_item:
+        return jsonify({"error": "Menu item ID already exists"}), 400
 
-    menu.append(new_menu_item)
-
-    with open("menu.json", "w") as file:
-        json.dump(menu, file)
+    db.menu.insert_one(new_menu_item)
 
     return jsonify({"message": "Menu item created successfully"}), 201
 
@@ -46,37 +55,31 @@ def create_menu_item():
 def update_menu_item(item_id):
     updated_menu_item = request.get_json()
 
-    with open("menu.json", "r") as file:
-        menu = json.load(file)
+    if "name" not in updated_menu_item or "price" not in updated_menu_item or "availability" not in updated_menu_item:
+        return jsonify({"error": "Invalid menu item data"}), 400
 
-    for item in menu:
-        if item["id"] == item_id:
-            item.update(updated_menu_item)
+    menu_item = db.menu.find_one({"id": item_id})
 
-            with open("menu.json", "w") as file:
-                json.dump(menu, file)
+    if menu_item:
+        menu_item["name"] = updated_menu_item["name"]
+        menu_item["price"] = updated_menu_item["price"]
+        menu_item["availability"] = updated_menu_item["availability"]
 
-            return jsonify({"message": "Menu item updated successfully"}), 200
+        db.menu.update_one({"id": item_id}, {"$set": menu_item})
 
-    return jsonify({"error": "Menu item not found"}), 404
+        return jsonify({"message": "Menu item updated successfully"}), 200
+    else:
+        return jsonify({"error": "Menu item not found"}), 404
 
 
 @app.route("/menu/delete/<int:item_id>", methods=["DELETE"])
 def delete_menu_item(item_id):
+    result = db.menu.delete_one({"id": item_id})
 
-    with open("menu.json", "r") as file:
-        menu = json.load(file)
-
-    for item in menu:
-        if item["id"] == item_id:
-            menu.remove(item)
-
-            with open("menu.json", "w") as file:
-                json.dump(menu, file)
-
-            return jsonify({"message": "Menu item deleted successfully"}), 200
-
-    return jsonify({"error": "Menu item not found"}), 404
+    if result.deleted_count > 0:
+        return jsonify({"message": "Menu item deleted successfully"}), 200
+    else:
+        return jsonify({"error": "Menu item not found"}), 404
 
 
 @app.route("/order", methods=["POST"])
@@ -88,17 +91,18 @@ def take_order():
 
     ordered_dishes = []
 
-    with open("menu.json", "r") as file:
-        menu = json.load(file)
-
     for dish_id in order_details["dish_ids"]:
-        dish = next((item for item in menu if item["id"] == dish_id), None)
+        dish = db.menu.find_one({"id": dish_id})
         if dish is None:
             return jsonify({"error": f"Dish with ID {dish_id} not found"}), 404
         elif not dish["availability"]:
             return jsonify({"error": f"Dish {dish['name']} is not available"}), 400
         else:
-            ordered_dishes.append(dish)
+            ordered_dishes.append({
+                "id": dish["id"],
+                "name": dish["name"],
+                "price": dish["price"]
+            })
 
     order = {
         "order_id": random.randint(100, 999),
@@ -107,43 +111,38 @@ def take_order():
         "status": "received"
     }
 
-    with open("orders.json", "r") as file:
-        orders = json.load(file)
-
-    orders.append(order)
-
-    with open("orders.json", "w") as file:
-        json.dump(orders, file)
+    db.orders.insert_one(order)
 
     return jsonify({"message": "Order placed successfully"}), 201
 
 
 @app.route("/review_orders", methods=["GET"])
 def review_orders():
-
-    with open("orders.json", "r") as file:
-        orders = json.load(file)
-
-    return jsonify(orders), 200
+    orders = list(db.orders.find())
+    orders_list = []
+    for item in orders:
+        orders_list.append({
+            'order_id': item['order_id'],
+            'customer_name': item['customer_name'],
+            'dishes': item['dishes'],
+            'status': item['status']
+        })
+    return jsonify(orders_list), 200
 
 
 @app.route("/order/update/<int:order_id>", methods=["PATCH"])
 def update_order_status(order_id):
     updated_status = request.get_json().get("status")
 
-    with open("orders.json", "r") as file:
-        orders = json.load(file)
+    result = db.orders.update_one(
+        {"order_id": order_id},
+        {"$set": {"status": updated_status}}
+    )
 
-    for order in orders:
-        if order["order_id"] == order_id:
-            order["status"] = updated_status
-
-            with open("orders.json", "w") as file:
-                json.dump(orders, file)
-
-            return jsonify({"message": "Order status updated successfully"}), 200
-
-    return jsonify({"error": "Order not found"}), 404
+    if result.modified_count > 0:
+        return jsonify({"message": "Order status updated successfully"}), 200
+    else:
+        return jsonify({"error": "Order not found"}), 404
 
 
 if __name__ == "__main__":
